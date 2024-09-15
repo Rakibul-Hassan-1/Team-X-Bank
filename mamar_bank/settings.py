e8fcdcfedb2a81f4526913aad039f9ee0c916318
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-09-15 11:44:42 +0600

    database added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -73,28 +73,28 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
 
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Feel free to alter this value to suit your needs.
-#         default='postgres://mamarbank_user:LALgYwd0AYd1wgoh3tFD2Mx6wa1tR4Rs@dpg-cm551lmd3nmc73am8kt0-a.oregon-postgres.render.com/mamarbank',
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://mamar_bank_7kji_user:XCKg7dUgVnVUiJCc1uvO81d5ClirRtws@dpg-crj77hij1k6c73fk13sg-a.oregon-postgres.render.com/mamar_bank_7kji',
+    )
+}
 
 
 # Password validation
